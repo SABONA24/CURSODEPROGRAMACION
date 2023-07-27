ca686266bf5a41e42e3ddc322f494918db8ffc1c
Sofia Bona <sofiabona2492@gmail.com>
2023-07-26 22:14:31 -0300

    app modificada

--- M3U4/app.js
+++ M3U4/app.js
@@ -8,6 +8,7 @@ var session = require('express-session');
 
 
 var indexRouter = require('./routes/index');
+const { log, Console } = require('console');
 //var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,12 +24,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret:'visualstudiocode',
+  secret: 'visualstudiocode',
   resave: false,
   saveUninitialized: true
 }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   var conocido = Boolean(req.session.nombre);
 
   res.render('index', {
@@ -39,32 +40,32 @@ app.get('/', function(req, res) {
 
 });
 
-app.get('/ingresar', function (req, res) {
-
+app.post('/ingresar', function (req, res) {
+  console.log(req.body.nombre)
   if (req.body.nombre) {
     req.session.nombre = req.body.nombre
-} 
-res.redirect('/');
+  }
+  res.redirect('/');
 });
 
 app.get('/salir', function (req, res) {
   req.session.destroy();
   res.redirect('/');
 });
-  
-  
+
+
 
 
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
