66790534e2b1f5fb9399b917a00f78fc41149f5c
Sofia Bona <sofiabona2492@gmail.com>
2023-08-22 22:11:56 -0300

    m5u2

--- sitio cerveceria/models/novedadesModel.js
+++ sitio cerveceria/models/novedadesModel.js
@@ -13,5 +13,33 @@ async function getNovedades() {
         return rows;
     } 
 
+    async function insertNovedad(obj) {
+        try {
+            var query = "inser into novedades set ?";
+            var rows = await pool.query(query, [obj])
+            return rows;
 
-module.exports ={ getNovedades, deleteNovedadesById } 
+        }catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async function getNovedadesById(id) {
+        var query = "select * from novedades where id=?";
+        var rows = await pool.query(query, [id]);
+        return rows [0];
+    }
+
+    async function modificarNovedadById(obj, id) {
+        try {
+            var query = "update novedades set ? where id=? ";
+            var rows = await pool.query(query,[obj, id]);
+            return rows;     
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+module.exports ={ getNovedades, deleteNovedadesById, insertNovedad, getNovedadesById, modificarNovedadById } 
